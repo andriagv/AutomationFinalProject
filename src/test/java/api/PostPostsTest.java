9e01509a88c7e17ec0e21f7f570e56c358734afb
andria.gv <gvaramiaandria1@gmail.com>
2025-06-25 12:00:15 +0400

    modified: PostPostsTest.java class

--- src/test/java/api/PostPostsTest.java
+++ src/test/java/api/PostPostsTest.java
@@ -1,4 +1,30 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PostPostsTest {
+
+    @Test
+    public void createPost_shouldReturn201_andCorrectBody() {
+        String requestBody = """
+                {
+                  "title": "foo",
+                  "body": "bar",
+                  "userId": 1
+                }
+                """;
+
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post("https://jsonplaceholder.typicode.com/posts");
+
+        Assert.assertEquals(response.statusCode(), 201);
+        Assert.assertEquals(response.jsonPath().getString("title"), "foo");
+        Assert.assertEquals(response.jsonPath().getInt("userId"), 1);
+    }
 }
