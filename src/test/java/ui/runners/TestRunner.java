c11446b20009eba941852a1bdda301491517bd61
andria.gv <gvaramiaandria1@gmail.com>
2025-06-25 12:31:19 +0400

    modified: ui runners TestRunner file

--- src/test/java/ui/runners/TestRunner.java
+++ src/test/java/ui/runners/TestRunner.java
@@ -1,4 +1,17 @@
 package ui.runners;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"ui.steps"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-html-reports.html",
+                "json:target/cucumber-reports/Cucumber.json"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
