15bdfb4225fdecc028336716cb9596b7fd72d4f6
andria.gv <gvaramiaandria1@gmail.com>
2025-06-25 12:01:35 +0400

    modified: DeletePostsTest.java class

--- src/test/java/api/DeletePostsTest.java
+++ src/test/java/api/DeletePostsTest.java
@@ -1,4 +1,17 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DeletePostsTest {
+
+    @Test
+    public void deletePost_shouldReturn200or204() {
+        Response response = RestAssured.delete("https://jsonplaceholder.typicode.com/posts/1");
+
+        int status = response.statusCode();
+        Assert.assertTrue(status == 200 || status == 204, "Unexpected status code: " + status);
+    }
 }
