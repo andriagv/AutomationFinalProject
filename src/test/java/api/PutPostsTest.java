6c6c2aa8b1ebb37ad2864a2723f5f903aab9c153
andria.gv <gvaramiaandria1@gmail.com>
2025-06-25 12:00:58 +0400

    modified: PutPostsTest.java class

--- src/test/java/api/PutPostsTest.java
+++ src/test/java/api/PutPostsTest.java
@@ -1,4 +1,30 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PutPostsTest {
+
+    @Test
+    public void updatePost_shouldChangeTitleField() {
+        String updatedBody = """
+                {
+                  "id": 1,
+                  "title": "updated title",
+                  "body": "bar",
+                  "userId": 1
+                }
+                """;
+
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(updatedBody)
+                .put("https://jsonplaceholder.typicode.com/posts/1");
+
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertEquals(response.jsonPath().getString("title"), "updated title");
+    }
 }
